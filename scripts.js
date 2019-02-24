d04c5d466b354805c5e5a00d22414a03b0726e99
Leilani Leach <leilanileach@yahoo.com>
2019-02-24 07:10:22 -0800

    create reducer and functions to display next lyric on click

--- scripts.js
+++ scripts.js
@@ -7,13 +7,19 @@ const initialState = {
   arrayPosition: 0,
 }
 
-//JEST TESTS AND SETUP
-const { expect } = window;
-expect(reducer(initialState, { type: null })).toEqual(initialState);
-
 //REDUX REDUCER
 const reducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case 'NEXT_LYRIC':
+      let newArrayPosition = state.arrayPosition + 1;
+      let newState = {
+        songLyricsArray: state.songLyricsArray,
+        arrayPosition: newArrayPosition,
+      }
+      return newState;
+    default:
+      return state;
+  }
 }
 
 // REDUX STORE
@@ -21,7 +27,34 @@ const { createStore } = Redux;
 const store = createStore(reducer);
 console.log(store.getState());
 
+//RENDERING STATE IN DOM
+const renderLyrics = () => {
+  const lyricsDisplay = document.getElementById('lyrics');
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
+  const renderedLine = document.createTextNode(currentLine);
+  document.getElementById('lyrics').appendChild(renderedLine);
+  window.onload = function () {
+    renderLyrics();
+  }
+}
+
+//JEST TESTS AND SETUP
+const { expect } = window;
+expect(reducer(initialState, { type: null })).toEqual(initialState);
+
+expect(reducer(initialState, { type: 'NEXT_LYRIC' })).toEqual({
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 1
+})
+
 // CLICK LISTENER
 const userClick = () => {
-  console.log('click');
+  store.dispatch({ type: 'NEXT_LYRIC' });
+  console.log(store.getState());
 }
+
+//SUBSCRIBE TO REDUX STORE
+store.subscribe(renderLyrics);
